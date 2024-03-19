e9d5cb9ba7442d18161fb07fdadf504dda47275b
etowahadams <etowahadams@gmail.com>
2024-03-19 12:28:22 -0400

    add example

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # PMC Figure Downloader
 
 This is a simple script to download figures from open access papers in [PubMed Central](https://www.ncbi.nlm.nih.gov/pmc/) database. It uses the [PMC API](https://www.ncbi.nlm.nih.gov/pmc/tools/developers/) to search for articles and uses the 
-[PMC Open Access Web Service API](https://www.ncbi.nlm.nih.gov/pmc/tools/oa-service/) to get the XML of each paper to determine the figure URLs.
+[PMC Open Access Web Service API](https://www.ncbi.nlm.nih.gov/pmc/tools/oa-service/) to get the XML of each paper to determine the figure information and URLs.
 
 ## Installation
 You can install using pip:
@@ -11,4 +11,49 @@ source env/bin/activate
 pip install -r requirements.txt
 ```
 
+## Usage
+See `example.ipynb` for a Jupyter notebook example.
 
+### Get paper IDs from a search query
+```python
+query = '"Nature Genetics"[Journal] AND "open access"[filter]'
+# a list of paper IDs
+result_ids = search_pmc(query, email="your-email-here@gmail.com", max_results=10)
+```
+### Extract figure information and URLs from a paper
+```python
+figure_data = extract_pmc_figures(result_ids)
+figure_data.write_parquet("figure_data.parquet")
+```
+The `figure_data` dataframe looks like this:
+```
+┌──────────┬────────┬───────────┬──────────────────────┬─────────────────────┬─────────────────────┐
+│ pmcid    ┆ fig_id ┆ fig_label ┆ fig_title            ┆ fig_desc            ┆ image_url           │
+│ ---      ┆ ---    ┆ ---       ┆ ---                  ┆ ---                 ┆ ---                 │
+│ str      ┆ str    ┆ str       ┆ str                  ┆ str                 ┆ str                 │
+╞══════════╪════════╪═══════════╪══════════════════════╪═════════════════════╪═════════════════════╡
+│ 10937393 ┆ Fig1   ┆ Fig. 1    ┆ FANS-based isolation ┆ a, Schematic        ┆ https://www.ncbi.nl │
+│          ┆        ┆           ┆ of nuclei o…         ┆ representation of   ┆ m.nih.gov/pmc…      │
+│          ┆        ┆           ┆                      ┆ t…                  ┆                     │
+│ 10937393 ┆ Fig2   ┆ Fig. 2    ┆ Purity and           ┆ a, Heatmaps depict  ┆ https://www.ncbi.nl │
+│          ┆        ┆           ┆ reproducibility of   ┆ log2-transfor…      ┆ m.nih.gov/pmc…      │
+│          ┆        ┆           ┆ th…                  ┆                     ┆                     │
+```
+
+### Download figures to a directory
+```python
+output_dir = "img"
+download_status = download_imgs(figure_data, output_dir)
+print("Failed downloads:")
+download_status.filter(pl.col("status") != 200)
+```
+The `download_status` dataframe will look like this:
+```
+┌──────────┬────────┬────────┐
+│ pmcid    ┆ fig_id ┆ status │
+│ ---      ┆ ---    ┆ ---    │
+│ str      ┆ str    ┆ i64    │
+╞══════════╪════════╪════════╡
+│ 10937393 ┆ Fig1   ┆ 200    │
+│ 10937393 ┆ Fig2   ┆ 200    │
+```
--- main.py
+++ main.py
@@ -1,62 +1,166 @@
 from Bio import Entrez
 import requests
 import xml.etree.ElementTree as ET
+import codecs
+import polars as pl
+import time
 
-headers = {
-    'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
-    'Referer': 'https://www.ncbi.nlm.nih.gov/pmc/articles/',
-    'sec-ch-ua-mobile': '?0',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
-    'sec-ch-ua-platform': '"macOS"',
-}
 
-def download_pmc_papers(query: str, retmax=10, email="your_email@example.com") -> list[list[str, bytes]]:
+def fix_cp1252_to_utf8(text):
     """
-    Downloads the XML content of PMC papers matching the given query.
+    Converts text that was originally encoded in CP-1252 but was read and sent as UTF-8,
+    back to its original form. This function was written by Claude.
 
     Args:
-        query (str): The search query for PMC papers.
-        retmax (int, optional): The maximum number of papers to retrieve (default: 10).
-        email (str, optional): The email address for Entrez (required for using the API).
+        text (str): The input text that needs to be fixed.
 
     Returns:
-        list: A list of strings containing the XML content of the retrieved papers.
+        str: The fixed text, encoded in UTF-8.
     """
+    try:
+        # Try decoding the text as UTF-8 first
+        decoded_text = text.encode("latin-1").decode("utf-8")
+    except UnicodeDecodeError:
+        # If decoding as UTF-8 fails, try decoding as CP-1252
+        decoded_text = codecs.decode(text, "cp1252")
+
+    # Re-encode the decoded text as UTF-8
+    fixed_text = decoded_text.encode("utf-8", errors="replace")
+
+    return fixed_text.decode("utf-8")
+
+
+ns = {"": "https://jats.nlm.nih.gov/ns/archiving/1.3/"}
+
+
+def get_text_or_empty(element, xpath):
+    node = element.find(xpath, namespaces=ns)
+    text_content = ET.tostring(node, method="text", encoding="unicode")
+    return text_content if node is not None else ""
+
+# TODO: Determine which of these are necessary
+headers = {
+    "sec-ch-ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+    "Referer": "https://www.ncbi.nlm.nih.gov/pmc/articles/",
+    "sec-ch-ua-mobile": "?0",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    "sec-ch-ua-platform": '"macOS"',
+}
+
+
+def extract_pmc_figures(ids: list[str]) -> pl.DataFrame:
+    """
+    Downloads the papers from PMC and extracts the figure data from the XML.
+
+    Args:
+        ids (list[str]): A list of PMCID's.
+
+    Returns:
+        pl.DataFrame: A DataFrame containing the figure data.
+    """
+
+    data = {
+        "pmcid": [],
+        "fig_id": [],
+        "fig_label": [],
+        "fig_title": [],
+        "fig_desc": [],
+        "image_url": [],
+    }
+
+    for i, pmcid in enumerate(ids):
+        time.sleep(0.2) # Be nice to the server
+        print(f"Getting paper {i + 1} of {len(ids)}...")
+
+        url = f"https://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi?verb=GetRecord&identifier=oai:pubmedcentral.nih.gov:{pmcid}&metadataPrefix=pmc"
+        response = requests.get(url)
+        # Fix the encoding of the response text
+        response_text = fix_cp1252_to_utf8(response.text)
+        # Get the XML root element
+        root = ET.fromstring(response_text)
+        # Find all the graphic elements in the XML
+        figures = root.findall(".//fig", namespaces=ns)
+        for figure in figures:
+            caption = get_text_or_empty(figure, ".//caption/p")
+            title = get_text_or_empty(figure, ".//caption/title")
+            label = get_text_or_empty(figure, ".//label")
+            id = figure.attrib["id"]
+
+            graphic = figure.find(
+                ".//graphic",
+                namespaces=ns,
+            )
+            if graphic is not None:
+                xlink_href = graphic.attrib["{http://www.w3.org/1999/xlink}href"]
+                image_url = f"https://www.ncbi.nlm.nih.gov/pmc/articles/PMC{pmcid}/bin/{xlink_href}.jpg"
+
+                data["pmcid"].append(pmcid)
+                data["fig_label"].append(label)
+                data["fig_desc"].append(caption)
+                data["fig_title"].append(title)
+                data["image_url"].append(image_url)
+                data["fig_id"].append(id)
+
+            else:
+                print("No graphic element found in the XML")
+
+    return pl.DataFrame(data)
+
+
+def download_imgs(data: pl.DataFrame, dir: str) -> pl.DataFrame:
+    status = {
+        "pmcid": [],
+        "fig_id": [],
+        "status": [],
+    }
+    for i, url in enumerate(data["image_url"]):
+        print(f"Downloading image {i + 1} of {len(data)}...")
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            with open(f"{dir}/{data['pmcid'][i]}_{data['fig_id'][i]}.jpg", "wb") as f:
+                f.write(response.content)
+        else:
+            print(f"Failed to download image. Status code: {response.status_code}")
+        status["pmcid"].append(data["pmcid"][i])
+        status["fig_id"].append(data["fig_id"][i])
+        status["status"].append(response.status_code)
+
+    return pl.DataFrame(status)
+
+
+def search_pmc(query: str, email: str, max_results: int = None, retmax: int = 100):
+    """
+    Searches PMC for papers matching the given query.
+    Returns a list of PMCID's.
+    """
+    if max_results and max_results < retmax:
+        retmax = max_results
+    
     Entrez.email = email
     handle = Entrez.esearch(db="pmc", term=query, retmax=retmax)
     record = Entrez.read(handle)
-    ids = record["IdList"]
-
-    image_data: list[list[str, bytes]] = []
-    for pmcid in ids[:1]:
-        url = f"https://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi?verb=GetRecord&identifier=oai:pubmedcentral.nih.gov:{pmcid}&metadataPrefix=pmc"
-        response = requests.get(url)
-        # Get the XML root element
-        root = ET.fromstring(response.text)
-        # Find all the graphic elements in the XML
-        graphics = root.findall(
-            ".//graphic", namespaces={"": "https://jats.nlm.nih.gov/ns/archiving/1.3/"}
+    print("Found", record["Count"], "papers. Getting their IDs...")
+    ids = []
+    for i in range(int(record["Count"]) // retmax + 1):
+        handle = Entrez.esearch(
+            db="pmc", term=query, retmax=retmax, retstart=i * retmax
         )
-        # Iterate over the graphic elements and download the images
-        for graphic in graphics:
-            xlink_href = graphic.attrib["{http://www.w3.org/1999/xlink}href"]
-            image_url = f"https://www.ncbi.nlm.nih.gov/pmc/articles/PMC{pmcid}/bin/{xlink_href}.jpg"
-            # Download the image
-            response = requests.get(image_url, headers=headers)
-            # Check if the request was successful (status code 200)
-            if response.status_code == 200:
-                image_data.append([pmcid, response.content])
-                print("Image downloaded successfully!")
-            else:
-                print(f"Failed to download image. Status code: {response.status_code}")
+        record = Entrez.read(handle)
+        ids.extend(record["IdList"])
+        # If we've reached the maximum number of results, stop getting more IDs
+        if max_results and (i + 1) * retmax >= max_results:
+            break
+        time.sleep(0.2)
+    print("Finished getting", len(ids), "IDs.")
 
-    return image_data
+    return ids
 
 
 if __name__ == "__main__":
-    query = "covid-19"
-    image_data = download_pmc_papers(query, email="your-email-here@gmail.com")
-
-    for i, image in enumerate(image_data):
-        with open(f"image_{i}.jpg", "wb") as f:
-            f.write(image[1])
+    query = '"Nature Genetics"[Journal] AND "open access"[filter]'
+    ids = search_pmc(query, email="etowahadams@gmail.com", max_results=10, retmax=10)
+    figure_data = extract_pmc_figures(ids)
+    print(figure_data)
+    figure_data.write_parquet("figures.parquet")
+    # data = download_pmc_papers(query, email="your-email-here@gmail.com")
+    # download_imgs(data)
